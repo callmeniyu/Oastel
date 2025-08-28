a1346c042eaca296c825f48b3a669b99df033c7a
callmeniyu <getniyashere@gmail.com>
2025-08-28 21:52:31 +0530

    update time

--- app/(routes)/booking/transfer/[slug]/page.tsx
+++ app/(routes)/booking/transfer/[slug]/page.tsx
@@ -582,7 +582,7 @@ export default function BookingInfoPage() {
                     {transferDetails?.seatCapacity ||
                       transferDetails?.maximumPerson ||
                       "N/A"}{" "}
-                    units
+                    seats
                   </p>
                   <p className="text-sm text-desc_gray">
                     Price shown is for the whole vehicle (per vehicle).
