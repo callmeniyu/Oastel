89e7c1ca6bcdf9afb9d66342af6721d52ea2870e
callmeniyu <getniyashere@gmail.com>
2026-02-20 21:53:58 +0530

    add bank partners to footer and about

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { FaFacebookF } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaTiktok } from "react-icons/fa";
-import { FaTelegramPlane } from "react-icons/fa";
 import Link from "next/link";
+import Image from "next/image";
+import { BsBank2 } from "react-icons/bs";
 
 export default function Footer() {
   return (
@@ -120,25 +116,20 @@ export default function Footer() {
             <div className="flex flex-col md:flex-row items-center justify-center gap-6 text-xs">
               {/* Stripe */}
               <div className="flex items-center gap-2">
-                <svg
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M13.976 9.15c-2.172-.806-3.356-1.426-3.356-2.409 0-.831.683-1.305 1.901-1.305 2.227 0 4.515.858 6.09 1.631l.89-5.494C18.252.975 15.697 0 12.165 0 9.667 0 7.589.654 6.104 1.872 4.56 3.147 3.757 4.992 3.757 7.218c0 4.039 2.467 5.76 6.476 7.219 2.585.92 3.445 1.574 3.445 2.583 0 .98-.84 1.545-2.354 1.545-1.875 0-4.965-.921-6.99-2.109l-.9 5.555C5.175 22.99 8.385 24 11.714 24c2.641 0 4.843-.624 6.328-1.813 1.664-1.305 2.525-3.236 2.525-5.732 0-4.128-2.524-5.851-6.591-7.305z" />
-                </svg>
+                <Image
+                  src="/images/stripe-seeklogo.png"
+                  alt="Stripe"
+                  width={60}
+                  height={20}
+                  className="h-5 w-auto"
+                />
                 <span className="text-gray-300">Secure Online Payments</span>
               </div>
 
               {/* Banking Partners */}
               <div className="flex items-center gap-2">
-                <svg
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M2 8.5h20v2H2zm0 5h20v2H2zm8-10l10 4v2H2v-2l10-4zm0 18h12v2H2v-2h8z" />
-                </svg>
+                <BsBank2 className="w-4 h-4" />
+
                 <span className="text-gray-300">
                   Public Bank & Alliance Bank
                 </span>
